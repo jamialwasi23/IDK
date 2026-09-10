b4aca55f5d20f291f1225936acc97caf6ac6e6f9
JamiFunds <jamialwasi2013@gmail.com>
2026-09-10 19:35:26 +0100

    Fix class declaration syntax in database.js

--- database.js
+++ database.js
@@ -5,7 +5,7 @@
  * A lightweight in-memory database system with basic CRUD operations
  */
 
-class Database {
+class Database { 
   constructor(repositoryUrl = 'https://github.com/jamialwasi23/IDK') {
     this.data = {};
     this.repositoryUrl = repositoryUrl;
